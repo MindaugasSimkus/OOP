de5a591d9606ce0bd2ecd3089883fdbc8a0c5d0c
MindaugasSimkus <shimkus.mindaugas@gmail.com>
2017-07-17 14:44:04 +0300

    homework done

--- cars.php
+++ cars.php
@@ -8,8 +8,9 @@ class car {
 	public $colorVariety;
 	public $fuelTankVolume;
 	public $topSpeed;
+	public $fuelConsumption;
 
-	public function __construct($name, $model, $date, $weight, $color, $ftVolume, $tSpeed) {
+	public function __construct($name, $model, $date, $weight, $color, $ftVolume, $tSpeed, $fCons) {
 		$this->manufacturer = $name;
 		$this->model = $model;
 		$this->dateOfManufacture = $date;
@@ -17,18 +18,93 @@ class car {
 		$this->colorVariety = $color;
 		$this->fuelTankVolume = $ftVolume;
 		$this->topSpeed = $tSpeed;
+		$this->fuelConsumption = $fCons;
 	}
 
 	public function maxSpeed() {	
-		show("$this->manufacturer $this->model can go at $this->topSpeed kmph");
+		show("$this->manufacturer $this->model can go at $this->topSpeed kmph.");
 	}
 
 
 	public function age() {
 		$exploded_doMan = explode("-", $this->dateOfManufacture);
-		$age = (date("z") + 1) - date("z", mktime(0, 0, 0, $exploded_doMan[1], $exploded_doMan[2], $exploded_doMan[0])) + 1;
+		$age = abs((date("z") + 1) - date("z", mktime(0, 0, 0, $exploded_doMan[1], $exploded_doMan[2], $exploded_doMan[0])) + 1);
 		$year = 2017 - $exploded_doMan[0];
-		show("$this->manufacturer $this->model was manufactured $year years and $age days ago");
+		show("$this->manufacturer $this->model was manufactured $year years and $age days ago.");
 	}
 
+	public function weight() {
+		show("$this->manufacturer $this->model weights $this->weight kilos.");
+	}
+
+	public function travelDistance() {
+		$distance = $this->fuelTankVolume/$this->fuelConsumption*100;
+		show("$this->manufacturer $this->model can go $distance kilometres whithout refilling.");
+	}
+
+}
+
+class sportscar extends car {
+	public $to100kmph;
+
+	public function __construct($name, $model, $date, $weight, $color, $ftVolume, $tSpeed, $fCons, $to100) {
+		$this->manufacturer = $name;
+		$this->model = $model;
+		$this->dateOfManufacture = $date;
+		$this->weight = $weight;
+		$this->colorVariety = $color;
+		$this->fuelTankVolume = $ftVolume;
+		$this->topSpeed = $tSpeed;
+		$this->fuelConsumption = $fCons;
+		$this->to100kmph = $to100;
+	}
+
+	public function to100() {
+		show("$this->manufacturer $this->model reaches 100 kmph in $this->to100kmph s.");
+	}
+}
+
+class electriccar extends car {
+	public $chargeTime;
+	public $travelDistance;
+
+	public function __construct($name, $model, $date, $weight, $color, $ftVolume, $tSpeed, $fCons, $chrgTime, $trvlDist) {
+		$this->manufacturer = $name;
+		$this->model = $model;
+		$this->dateOfManufacture = $date;
+		$this->weight = $weight;
+		$this->colorVariety = $color;
+		$this->fuelTankVolume = $ftVolume;
+		$this->topSpeed = $tSpeed;
+		$this->fuelConsumption = $fCons;
+		$this->chargeTime = $chrgTime;
+		$this->travelDistance = $trvlDist;
+	}
+
+	public function travelDistance() {
+		show("$this->manufacturer $this->model can reach $this->travelDistance kilometres distance.");
+	}
+	public function chargeTime() {
+		show("$this->manufacturer $this->model electric car charges for $this->chargeTime hours.");
+	}
+}
+
+class tanker extends car {
+	public $tankVolume;
+
+	public function __construct($name, $model, $date, $weight, $color, $ftVolume, $tSpeed, $fCons, $tVol) {
+		$this->manufacturer = $name;
+		$this->model = $model;
+		$this->dateOfManufacture = $date;
+		$this->weight = $weight;
+		$this->colorVariety = $color;
+		$this->fuelTankVolume = $ftVolume;
+		$this->topSpeed = $tSpeed;
+		$this->fuelConsumption = $fCons;
+		$this->tankVolume = $tVol;
+	}
+
+	public function tanker() {
+		show("$this->manufacturer $this->model has a tank that can be filled up to $this->tankVolume litres of gas.");
+	}
 }
--- homework.php
+++ homework.php
@@ -3,24 +3,91 @@
 include "messages.php";
 include "cars.php";
 
-$BMW = new car ("BMW", "328", "2011-01-02", 1422, 5, 45, 280);
+$Audi = new sportscar ("Audi", "S4", "2013-05-29", 1322, 3, 55, 280, 10, 7.6);
+$BMW = new sportscar ("BMW", "M4", "2013-04-09", 1496, 5, 50, 295, 10.5, 7.2);
+$Ferrari = new sportscar ("Ferrari", "Spider", "2016-12-28", 1208, 2, 60, 310, 14, 6.9);
+
+
+showa($Audi);
+
+$Audi->maxSpeed();
+$Audi->age();
+$Audi->weight();
+$Audi->travelDistance();
+$Audi->to100();
 
 showa($BMW);
 
-$BMW-> maxSpeed();
-$BMW-> age();
+$BMW->maxSpeed();
+$BMW->age();
+$BMW->weight();
+$BMW->travelDistance();
+$BMW->to100();
+
+showa($Ferrari);
+
+$Ferrari->maxSpeed();
+$Ferrari->age();
+$Ferrari->weight();
+$Ferrari->travelDistance();
+$Ferrari->to100();
+
+$Tesla = new electriccar ("Tesla", "Model S", "2016-04-17", 1926, 7, 0, 220, 0, 6, 700);
+$Toyota = new electriccar ("Toyota", "Prius", "2014-04-09", 1736, 5, 30, 165, 3, 8, 120);
+$eGolf = new electriccar ("Volkswagen", "eGolf", "2017-02-18", 1639, 1, 0, 180, 0, 7, 400);
+
+showa($Tesla);
+
+$Tesla->maxSpeed();
+$Tesla->age();
+$Tesla->weight();
+$Tesla->travelDistance();
+$Tesla->chargeTime();
+
+showa($Toyota);
+
+$Toyota->maxSpeed();
+$Toyota->age();
+$Toyota->weight();
+$Toyota->travelDistance();
+$Toyota->chargeTime();
+
+showa($eGolf);
+
+$eGolf->maxSpeed();
+$eGolf->age();
+$eGolf->weight();
+$eGolf->travelDistance();
+$eGolf->chargeTime();
+
+$Man = new tanker ("MAN", "380", "2008-07-01", 9621, 2, 180, 150, 16, 12000);
+$Volvo = new tanker ("Volvo", "T800", "2015-05-09", 8200, 6, 220, 190, 15.6, 15000);
+$Zyl = new tanker ("Lada", "ZYL", "1979-02-14", 11253, 1, 100, 95, 32.6, 7000);
 
 
+showa($Man);
 
+$Man->maxSpeed();
+$Man->age();
+$Man->weight();
+$Man->travelDistance();
+$Man->tanker();
 
+showa($Volvo);
 
+$Volvo->maxSpeed();
+$Volvo->age();
+$Volvo->weight();
+$Volvo->travelDistance();
+$Volvo->tanker();
 
+showa($Zyl);
 
-
-
-
-
-
+$Zyl->maxSpeed();
+$Zyl->age();
+$Zyl->weight();
+$Zyl->travelDistance();
+$Zyl->tanker();
 
 
 
